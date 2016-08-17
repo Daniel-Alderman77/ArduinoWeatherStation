439d21c38975a858564b8eadccc18f964252d242
Daniel Alderman <daniel.alderman77@gmail.com>
2016-08-17 16:43:26 +0100

    Added try and catch for when the serial signal cannot be read

--- Client/ReadThermometer.py
+++ Client/ReadThermometer.py
@@ -4,21 +4,25 @@ import sys
 temp_array = []
 
 def read_thermometer():
-    ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+    try:
+        ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
 
-    while True:
-        temp_str = ser.readline()
+        while True:
+            temp_str = ser.readline()
 
-        # Remove unecessary character from string leaving just temperature
-        temp = temp_str[0:5]
+            # Remove unecessary character from string leaving just temperature
+            temp = temp_str[0:5]
 
-        print temp
+            print temp
 
-        sys.stdout.write(temp)
-        temp_array.append(temp)
+            sys.stdout.write(temp)
+            temp_array.append(temp)
 
-        print temp_array
+            print temp_array
 
-        sys.stdout.flush()
+            sys.stdout.flush()
+
+    except serial.serialutil.SerialException:
+        print "Could not detect Arduino on port: /dev/cu.usbmodem1411"
 
 read_thermometer()
