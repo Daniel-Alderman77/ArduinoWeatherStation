52c32f9073ed4a68e9362013040f4cb0b929d4bb
Daniel Alderman <daniel.alderman77@gmail.com>
2017-01-24 20:55:57 +0000

    Read and print the raw analog value over serial

--- Client/ReadThermometer.py
+++ Client/ReadThermometer.py
@@ -2,7 +2,7 @@ import serial
 import sys
 
 temp_array = []
-arduino_string = '/dev/cu.usbmodem1451'
+arduino_string = '/dev/cu.usbmodem1421'
 
 
 def read_thermometer():
@@ -12,19 +12,22 @@ def read_thermometer():
         while True:
             temp_str = ser.readline()
 
+            print temp_str
+
             # Remove unecessary character from string leaving just temperature
-            temp = temp_str[0:5]
-
-            print temp
-
-            sys.stdout.write(temp)
-            temp_array.append(temp)
-
-            print temp_array
+            # temp = temp_str[0:5]
+            #
+            # print temp
+            #
+            # sys.stdout.write(temp)
+            # temp_array.append(temp)
+            #
+            # print temp_array
 
             sys.stdout.flush()
 
-    except serial.serialutil.SerialException:
+    except serial.serialutil.SerialException as e:
+        print e
         print ("Could not detect Arduino on port: " + arduino_string)
 
 read_thermometer()
